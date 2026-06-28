50942ce387cda3eb9e497af3025848e31ec5101f
Daniel <daniel@platoniq.health>
2026-06-28 21:42:22 +0200

    auto update pwa on re-deploy

--- resources/public/sw.js
+++ resources/public/sw.js
@@ -1,1 +1,41 @@
-self.addEventListener('fetch', () => {});
+const CACHE = 'strichliste-__CACHE_VERSION__';
+
+const PRECACHE = ['/', '/index.html', '/js/compiled/app.js', '/icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(PRECACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(res => {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(event.request, copy));
+          return res;
+        })
+        .catch(() => caches.match('/index.html'))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(cached =>
+        cached || fetch(event.request).then(res => {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(event.request, copy));
+          return res;
+        })
+      )
+    );
+  }
+});
